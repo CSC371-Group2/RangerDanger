7dc70d5ef514027bbd10c3defdc6186b019b1994
Griffin Johnson <cougargriff@gmail.com>
2020-02-17 20:00:56 -0800

    griffin initial push

---
+++ Park Ranger/Assets/griffin/scripts/Flicker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flicker : MonoBehaviour
+{
+    Light lght;
+    
+    float duration;
+    public Color r1;
+    public Color r2;
+    public float flicker_delta = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lght = GetComponent<Light>();     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // set light color
+        duration = Random.Range(0f, flicker_delta);
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        lght.color = Color.Lerp(r1, r2, t);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RangerDanger
 Ranger Danger Official Repo
+
+ ## Unity Version: 2019.2.17f1
+
+ __After you cloned this repo, use Unity Hub to open the project in folder "Park Ranger", _NOT_ "RangerDanger"__
