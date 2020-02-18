a5aafea0be3b90984a5e693107c08dffdbf9a790
Matthew Jaojoco <mjaojoco@calpoly.edu>
2020-02-18 10:20:32 -0800

    oilSlider added

---
+++ Park Ranger/Assets/Scripts - Put all scripts here/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Slider oilSlider;
+    public float maxOil = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oilSlider.maxValue = maxOil;
+        oilSlider.minValue = 0;
+        oilSlider.value = maxOil;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oilSlider.value -= (2 * Time.deltaTime);
+    }
+}
