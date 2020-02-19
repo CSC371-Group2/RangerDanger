bd1042d6ce3cff795c1093d902d8efd942601d67
Toan Pham <tpham61@calpoly.edu>
2020-02-18 18:14:22 -0800

    Adding PlayerMovement and PlayerFollow script
    Changing the Player to a capsule with collider
    Added PlayerMovement script
    Added PlayerFollow script
    Now when the player get close enough in front of the camper, the camper will start following the player

--- Park Ranger/Assets/Scripts - Put all scripts here/Camper/PlayerFollow.cs
+++ Park Ranger/Assets/Scripts - Put all scripts here/Camper/PlayerFollow.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform player;
+    public float speed;
+    public float stoppingDistance;
+    public float detectDistance;
+    public LayerMask playerLayer;            //Layer on which collision will be checked.
+
+
+    public bool foundPlayer = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 startPos = transform.position;
+        Vector3 endPos = startPos + new Vector3(detectDistance, 0, 0);
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if ((Physics.Linecast(startPos, endPos, playerLayer)) && !foundPlayer)
+        {
+            foundPlayer = true;
+        }
+        if (foundPlayer)
+        {
+            if (distanceToPlayer > stoppingDistance)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            }
+        }
     }
 }
