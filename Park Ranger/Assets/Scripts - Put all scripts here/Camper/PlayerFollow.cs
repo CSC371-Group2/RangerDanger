274a670f0b5b8185bd6e8055ac3b33a4267f8a30
twambolt <terry.wambolt38@gmail.com>
2020-03-18 18:23:27 -0700

    Got the camper to rotate and float

--- Park Ranger/Assets/Scripts - Put all scripts here/Camper/PlayerFollow.cs
+++ Park Ranger/Assets/Scripts - Put all scripts here/Camper/PlayerFollow.cs
@@ -9,8 +9,11 @@ public class PlayerFollow : MonoBehaviour
     private float detectDistance = GameSettings.detectDist;
     private Transform player;
     private float speed = GameSettings.lerpSpeed; //between 0 and 1;
+    public float turnSpeed = GameSettings.lerpSpeed;
     private float stoppingDistance = GameSettings.stopDist;
     private bool foundPlayer = false;
+    private GameObject playerPos;
+    private Animator camperAnimator;
 
     private GameManager gm;
 
@@ -19,30 +22,53 @@ public class PlayerFollow : MonoBehaviour
     {
         gm = GameManager.instance;
         player = GameObject.Find("Ranger D. Danger").GetComponent<Transform>();
+        playerPos = GameObject.Find("Ranger D. Danger");
+        camperAnimator = GetComponent<Animator>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
         Vector3 startPos = transform.position;
         Vector3 endPos = startPos + new Vector3(detectDistance, 0, 0);
+        // detecting player
         if ((Physics.Linecast(startPos, endPos, playerLayer)) && !foundPlayer)
         {
             gm.is_camper_following = true;
             GameManager.instance.UpdateObjectives(gm.is_camper_following);
             foundPlayer = true;
         }
-    }
 
-    private void FixedUpdate()
-    {
-        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        // moving camper
         if (foundPlayer)
         {
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
             if (distanceToPlayer > stoppingDistance)
             {
+                //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(playerPos.transform.position), turnSpeed);
+                transform.LookAt(player, Vector3.up);
                 transform.position = Vector3.Lerp(transform.position, player.position, speed);
+                camperAnimator.SetBool("isRunning", true);
             }
+            else
+            {
+                camperAnimator.SetBool("isRunning", false);
+            }
+            
         }
     }
+
+    //private void LateUpdate()
+    //{
+        
+    //    if (foundPlayer)
+    //    {
+    //        if (distanceToPlayer > stoppingDistance)
+    //        {
+    //            transform.position = Vector3.Lerp(transform.position, player.position, speed);
+    //        }
+    //    }
+    //}
 }
